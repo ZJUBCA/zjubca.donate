9017d52edb13c85903d0e45e5e9ffbaaab4f8fcb
yyh1102 <yyh951102@163.com>
2019-04-20 09:02:58 +0800

    Updates

---
+++ precache-manifest.7ddbb7fe5075e167b00204c1728e5f71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "43defa2fbba3f99eda4d",
+    "url": "/zjubca.donate/static/css/main.9707c967.chunk.css"
+  },
+  {
+    "revision": "43defa2fbba3f99eda4d",
+    "url": "/zjubca.donate/static/js/main.b3170c50.chunk.js"
+  },
+  {
+    "revision": "f73da81fa1ce1aad5881",
+    "url": "/zjubca.donate/static/js/runtime~main.95d750c1.js"
+  },
+  {
+    "revision": "21a32a0f1a7b5d9af923",
+    "url": "/zjubca.donate/static/css/2.b7133d11.chunk.css"
+  },
+  {
+    "revision": "21a32a0f1a7b5d9af923",
+    "url": "/zjubca.donate/static/js/2.46aeff13.chunk.js"
+  },
+  {
+    "revision": "14cef9d374734e4702c2dbc16fefc5d8",
+    "url": "/zjubca.donate/static/media/logo_general.14cef9d3.png"
+  },
+  {
+    "revision": "4c39a41d91362ec69ac39dfefc4a87e3",
+    "url": "/zjubca.donate/index.html"
+  }
+];
